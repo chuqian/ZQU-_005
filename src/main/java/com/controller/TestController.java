c413ada0d91d503562250152a0583d16aa39b4fe
MaxBill13 <1126137275@qq.com>
2018-09-07 15:30:08 +0800

    页面存放到view文件夹下

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	{
+		System.out.println("ini....");
+	}
+	
+	@RequestMapping("/goodsPullOn.action")
+	public String goodsPullOn(){
+		return "/seller/goodsPullOn";
+	}
+	
+	
+}
