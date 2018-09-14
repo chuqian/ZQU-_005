7399e94a5a94fe7730ca409249897536bef8d97c
lgpeng <2630549550@qq.com>
2018-09-14 10:30:40 +0800

    update LoginController

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,18 +1,18 @@
-
 package com.controller;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.entity.User;
-import com.util.MD5;
+
+import com.realm.MyUsernamePasswordToken;
+import com.service.LoginService;
 
 /**
  *@author : lgpeng
@@ -20,26 +20,23 @@ import com.util.MD5;
  *@descriptioin :  处理登录请求
  */
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 	
+	@Autowired
+	private LoginService loginService;
 	private final String LOGIN_FIAL = "fail";
 	
 	@ResponseBody
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(User user, HttpServletResponse response, boolean rememberMe) throws IOException {
-		
+	@RequestMapping(value = "/{role}", method = RequestMethod.POST)
+	public String login(HttpServletRequest request, boolean rememberMe, @PathVariable(value="role")String role) throws IOException {
 		Subject subject = SecurityUtils.getSubject();
-		UsernamePasswordToken token = new UsernamePasswordToken(user.getId(), MD5.encode(user.getPassword(), user.getId()));
+		MyUsernamePasswordToken token = new MyUsernamePasswordToken(request.getParameter("logUsername"), request.getParameter("logPassword"), role);
 		token.setRememberMe(rememberMe);
 		
 		try {
 			subject.login(token);
-			if(subject.hasRole("admin"))		
-				return "admin";	
-			else if(subject.hasRole("user")) 
-				return "user";
-			else 
-				return "saller";
+			return loginService.getLoginSuccessUrl(role);
 		} catch (Exception e) {
 			return LOGIN_FIAL;
 		}
